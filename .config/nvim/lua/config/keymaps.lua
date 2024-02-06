1dcda2c9f89dd607526253264021464c96cef952
Lars Bollmann <55883538+LarsBollmann@users.noreply.github.com>
2024-02-06 15:17:26 +0100

    Update keymaps.lua

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,13 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
+
+local Util = require("lazyvim.util")
+local lazyterm = function()
+  Util.terminal(nil, { cwd = Util.root() })
+end
+
+vim.keymap.set("n", "<c-7>", lazyterm, { desc = "Terminal (root dir)" })
+vim.keymap.set("t", "<C-7>", "<cmd>close<cr>", { desc = "Hide Terminal" })
+
+vim.keymap.set("n", "<A-left>", "<cmd>bprevious<cr>", { desc = "Prev buffer" })
+vim.keymap.set("n", "<A-right>", "<cmd>bnext<cr>", { desc = "Next buffer" })
